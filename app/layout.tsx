653d63948757c53547a615f1aff95a9f627c3a0c
Yamoon224 <Yamooon664@gmail.com>
2025-05-07 16:15:12 +0000

    modified:   app/layout.tsx 	new file:   public/favicon.svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
